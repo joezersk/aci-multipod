6c500b47bfafd23f0bd48ddc3e5b3e5a4f9754d1
Joseph Ezerski <joezersk@cisco.com>
2016-08-18 13:41:30 +0200

    Python Scripts to help deploy multipod programatically

---
+++ mpod-base.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+# list of packages that should be imported for this code to work
+import cobra.mit.access
+import cobra.mit.request
+import cobra.mit.session
+import cobra.model.bgp
+import cobra.model.ctrlr
+import cobra.model.dbg
+import cobra.model.fabric
+import cobra.model.fv
+import cobra.model.l3ext
+import cobra.model.ospf
+import cobra.model.pol
+from cobra.internal.codec.xmlcodec import toXMLStr
+
+# log into an APIC and create a directory object
+ls = cobra.mit.session.LoginSession('https://10.50.129.241', 'admin', 'cisco123')
+md = cobra.mit.access.MoDirectory(ls)
+md.login()
+
+# the top level object on which operations will be made
+topMo = cobra.model.pol.Uni('')
+
+# build the request using cobra syntax
+fabricInst = cobra.model.fabric.Inst(topMo)
+dbgOngoingAcMode = cobra.model.dbg.OngoingAcMode(fabricInst, mode='path', name='default')
+
+c = cobra.mit.request.ConfigRequest()
+c.addMo(fabricInst)
+md.commit(c)
+
+fvTenant = cobra.model.fv.Tenant(topMo, name='infra')
+fvFabricExtConnP = cobra.model.fv.FabricExtConnP(fvTenant, rt='extended:as2-nn4:11:22', id='1', name='fabricExtConnPolName_9a92763eadbe259d')
+fvPodConnP = cobra.model.fv.PodConnP(fvFabricExtConnP, id='2')
+fvIp = cobra.model.fv.Ip(fvPodConnP, addr='200.1.1.1/32')
+fvPodConnP2 = cobra.model.fv.PodConnP(fvFabricExtConnP, id='1')
+fvIp2 = cobra.model.fv.Ip(fvPodConnP2, addr='100.1.1.1/32')
+l3extFabricExtRoutingP = cobra.model.l3ext.FabricExtRoutingP(fvFabricExtConnP, name='MPOD_EXT_ROUTE_PROFILE')
+l3extSubnet = cobra.model.l3ext.Subnet(l3extFabricExtRoutingP, ip='202.1.0.0/16')
+l3extSubnet2 = cobra.model.l3ext.Subnet(l3extFabricExtRoutingP, ip='203.1.0.0/16')
+fvPeeringP = cobra.model.fv.PeeringP(fvFabricExtConnP, type='automatic_with_full_mesh')
+ospfIfPol = cobra.model.ospf.IfPol(fvTenant, nwT='p2p', pfxSuppress='inherit', name='MPOD_OSPF_P2P', prio='1', ctrl='advert-subnet,mtu-ignore', helloIntvl='10', rexmitIntvl='5', xmitDelay='1', cost='unspecified', deadIntvl='40')
+l3extOut = cobra.model.l3ext.Out(fvTenant, name='MPOD_OSPF_OUT', enforceRtctrl='export', targetDscp='unspecified')
+l3extRsEctx = cobra.model.l3ext.RsEctx(l3extOut, tnFvCtxName='overlay-1')
+l3extLNodeP = cobra.model.l3ext.LNodeP(l3extOut, tag='yellow-green', name='MPOD_OSPF_NODES', targetDscp='unspecified')
+l3extRsNodeL3OutAtt = cobra.model.l3ext.RsNodeL3OutAtt(l3extLNodeP, tDn='topology/pod-2/node-202', rtrId='222.222.222.222', rtrIdLoopBack='yes')
+l3extInfraNodeP = cobra.model.l3ext.InfraNodeP(l3extRsNodeL3OutAtt, fabricExtCtrlPeering='yes')
+l3extLoopBackIfP = cobra.model.l3ext.LoopBackIfP(l3extRsNodeL3OutAtt, addr='222.222.222.222')
+l3extRsNodeL3OutAtt2 = cobra.model.l3ext.RsNodeL3OutAtt(l3extLNodeP, tDn='topology/pod-1/node-201', rtrId='111.111.111.111', rtrIdLoopBack='yes')
+l3extInfraNodeP2 = cobra.model.l3ext.InfraNodeP(l3extRsNodeL3OutAtt2, fabricExtCtrlPeering='yes')
+l3extLoopBackIfP2 = cobra.model.l3ext.LoopBackIfP(l3extRsNodeL3OutAtt2, addr='111.111.111.111')
+l3extLIfP = cobra.model.l3ext.LIfP(l3extLNodeP, tag='yellow-green', name='MPOD_OSPF_INTS')
+ospfIfP = cobra.model.ospf.IfP(l3extLIfP, authKeyId='1', authType='none')
+ospfRsIfPol = cobra.model.ospf.RsIfPol(ospfIfP, tnOspfIfPolName='MPOD_OSPF_P2P')
+l3extRsNdIfPol = cobra.model.l3ext.RsNdIfPol(l3extLIfP)
+l3extRsIngressQosDppPol = cobra.model.l3ext.RsIngressQosDppPol(l3extLIfP)
+l3extRsEgressQosDppPol = cobra.model.l3ext.RsEgressQosDppPol(l3extLIfP)
+l3extRsPathL3OutAtt = cobra.model.l3ext.RsPathL3OutAtt(l3extLIfP, tDn='topology/pod-2/paths-202/pathep-[eth1/20]', targetDscp='unspecified', encapScope='local', llAddr='::', mac='00:22:BD:F8:19:FF', mode='regular', encap='vlan-4', ifInstT='sub-interface', mtu='inherit', addr='203.1.1.1/30')
+l3extRsPathL3OutAtt2 = cobra.model.l3ext.RsPathL3OutAtt(l3extLIfP, tDn='topology/pod-1/paths-201/pathep-[eth1/20]', targetDscp='unspecified', encapScope='local', llAddr='::', mac='00:22:BD:F8:19:FF', mode='regular', encap='vlan-4', ifInstT='sub-interface', mtu='inherit', addr='202.1.1.1/30')
+l3extInstP = cobra.model.l3ext.InstP(l3extOut, matchT='AtleastOne', name='l3extInstPNamee75ff0551285bf11', prio='unspecified', targetDscp='unspecified')
+fvRsCustQosPol = cobra.model.fv.RsCustQosPol(l3extInstP)
+bgpExtP = cobra.model.bgp.ExtP(l3extOut)
+ospfExtP = cobra.model.ospf.ExtP(l3extOut, areaCtrl='redistribute,summary', areaType='regular', areaCost='1', areaId='backbone')
+
+c = cobra.mit.request.ConfigRequest()
+c.addMo(fvTenant)
+md.commit(c)
+
+
+ctrlrInst = cobra.model.ctrlr.Inst(topMo)
+fabricSetupPol = cobra.model.fabric.SetupPol(ctrlrInst, name='default')
+fabricSetupP = cobra.model.fabric.SetupP(fabricSetupPol, tepPool='10.1.0.0/16', podId='2')
+
+
+# commit the generated code to APIC
+#print toXMLStr(topMo)
+c = cobra.mit.request.ConfigRequest()
+c.addMo(ctrlrInst)
+md.commit(c)
+
---
+++ mpod-phys.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+# list of packages that should be imported for this code to work
+import cobra.mit.access
+import cobra.mit.request
+import cobra.mit.session
+import cobra.model.bgp
+import cobra.model.dhcp
+import cobra.model.fabric
+import cobra.model.fv
+import cobra.model.fvns
+import cobra.model.infra
+import cobra.model.l3ext
+import cobra.model.pol
+from cobra.internal.codec.xmlcodec import toXMLStr
+
+# log into an APIC and create a directory object
+ls = cobra.mit.session.LoginSession('https://10.50.129.241', 'admin', 'cisco123')
+md = cobra.mit.access.MoDirectory(ls)
+md.login()
+
+# the top level object on which operations will be made
+topMo = cobra.model.pol.Uni('')
+
+# build the request using cobra syntax
+infraInfra = cobra.model.infra.Infra(topMo)
+infraFuncP = cobra.model.infra.FuncP(infraInfra, name='default')
+infraSpAccPortGrp = cobra.model.infra.SpAccPortGrp(infraFuncP, name='MPOD-SPINE2-PG')
+infraRsCdpIfPol = cobra.model.infra.RsCdpIfPol(infraSpAccPortGrp)
+infraRsAttEntP = cobra.model.infra.RsAttEntP(infraSpAccPortGrp, tDn='uni/infra/attentp-MPOD_AAEP')
+infraRsHIfPol = cobra.model.infra.RsHIfPol(infraSpAccPortGrp)
+infraSpAccPortGrp2 = cobra.model.infra.SpAccPortGrp(infraFuncP, name='MPOD-SPINE1-PG')
+infraRsCdpIfPol2 = cobra.model.infra.RsCdpIfPol(infraSpAccPortGrp2)
+infraRsAttEntP2 = cobra.model.infra.RsAttEntP(infraSpAccPortGrp2, tDn='uni/infra/attentp-MPOD_AAEP')
+infraRsHIfPol2 = cobra.model.infra.RsHIfPol(infraSpAccPortGrp2)
+fvnsVlanInstP = cobra.model.fvns.VlanInstP(infraInfra, name='MPOD_VLANS', allocMode='static')
+fvnsEncapBlk = cobra.model.fvns.EncapBlk(fvnsVlanInstP, to='vlan-4', from_='vlan-4', allocMode='inherit')
+infraAttEntityP = cobra.model.infra.AttEntityP(infraInfra, name='MPOD_AAEP')
+infraProvAcc = cobra.model.infra.ProvAcc(infraAttEntityP, name='provacc')
+dhcpInfraProvP = cobra.model.dhcp.InfraProvP(infraProvAcc, mode='controller')
+infraRsFuncToEpg = cobra.model.infra.RsFuncToEpg(infraProvAcc, mode='regular', tDn='uni/tn-infra/ap-access/epg-default', encap='vlan-3456', primaryEncap='unknown')
+infraRsDomP = cobra.model.infra.RsDomP(infraAttEntityP, tDn='uni/l3dom-MPOD_L3_DOMAIN')
+infraSpineP = cobra.model.infra.SpineP(infraInfra, name='MPOD-S2-SWITCH')
+infraSpineS = cobra.model.infra.SpineS(infraSpineP, type='range', name='SPINE2')
+infraNodeBlk = cobra.model.infra.NodeBlk(infraSpineS, from_='202', name='05183e5184f05c0e', to_='202')
+infraRsSpAccPortP = cobra.model.infra.RsSpAccPortP(infraSpineP, tDn='uni/infra/spaccportprof-MPOD-S2-ETH20')
+infraSpineP2 = cobra.model.infra.SpineP(infraInfra, name='MPOD-S1-SWITCH')
+infraSpineS2 = cobra.model.infra.SpineS(infraSpineP2, type='range', name='SPINE1')
+infraNodeBlk2 = cobra.model.infra.NodeBlk(infraSpineS2, from_='201', name='7eb0b8ef28c813b6', to_='201')
+infraRsSpAccPortP2 = cobra.model.infra.RsSpAccPortP(infraSpineP2, tDn='uni/infra/spaccportprof-MPOD-S1-ETH20')
+infraSpAccPortP = cobra.model.infra.SpAccPortP(infraInfra, name='MPOD-S2-ETH20')
+infraSHPortS = cobra.model.infra.SHPortS(infraSpAccPortP, type='range', name='ETH1-20')
+infraPortBlk = cobra.model.infra.PortBlk(infraSHPortS, name='block2', fromPort='20', fromCard='1', toPort='20', toCard='1')
+infraRsSpAccGrp = cobra.model.infra.RsSpAccGrp(infraSHPortS, tDn='uni/infra/funcprof/spaccportgrp-MPOD-SPINE2-PG')
+infraSpAccPortP2 = cobra.model.infra.SpAccPortP(infraInfra, name='MPOD-S1-ETH20')
+infraSHPortS2 = cobra.model.infra.SHPortS(infraSpAccPortP2, type='range', name='ETH1_20')
+infraPortBlk2 = cobra.model.infra.PortBlk(infraSHPortS2, name='block2', fromPort='20', fromCard='1', toPort='20', toCard='1')
+infraRsSpAccGrp2 = cobra.model.infra.RsSpAccGrp(infraSHPortS2, tDn='uni/infra/funcprof/spaccportgrp-MPOD-SPINE1-PG')
+
+
+c = cobra.mit.request.ConfigRequest()
+c.addMo(infraInfra)
+md.commit(c)
+
+fvTenant = cobra.model.fv.Tenant(topMo, name='infra')
+l3extOut = cobra.model.l3ext.Out(fvTenant, name='MPOD_OSPF_OUT', enforceRtctrl='export', targetDscp='unspecified')
+l3extRsL3DomAtt = cobra.model.l3ext.RsL3DomAtt(l3extOut, tDn='uni/l3dom-MPOD_L3_DOMAIN')
+
+c = cobra.mit.request.ConfigRequest()
+c.addMo(fvTenant)
+md.commit(c)
+
+fabricInst = cobra.model.fabric.Inst(topMo)
+fabricFuncP = cobra.model.fabric.FuncP(fabricInst)
+fabricPodPGrp = cobra.model.fabric.PodPGrp(fabricFuncP, name='HOUSEKEEPING_GROUP')
+fabricRsPodPGrpIsisDomP = cobra.model.fabric.RsPodPGrpIsisDomP(fabricPodPGrp)
+fabricRsPodPGrpBGPRRP = cobra.model.fabric.RsPodPGrpBGPRRP(fabricPodPGrp, tnBgpInstPolName='default')
+fabricRsTimePol = cobra.model.fabric.RsTimePol(fabricPodPGrp)
+fabricRsCommPol = cobra.model.fabric.RsCommPol(fabricPodPGrp)
+fabricRsPodPGrpCoopP = cobra.model.fabric.RsPodPGrpCoopP(fabricPodPGrp)
+fabricRsSnmpPol = cobra.model.fabric.RsSnmpPol(fabricPodPGrp)
+bgpInstPol = cobra.model.bgp.InstPol(fabricInst, name='default')
+bgpRRP = cobra.model.bgp.RRP(bgpInstPol)
+bgpRRNodePEp = cobra.model.bgp.RRNodePEp(bgpRRP, id='202', podId='1')
+bgpRRNodePEp2 = cobra.model.bgp.RRNodePEp(bgpRRP, id='201', podId='1')
+bgpAsP = cobra.model.bgp.AsP(bgpInstPol, asn='65001')
+fabricPodP = cobra.model.fabric.PodP(fabricInst, name='default')
+fabricPodS = cobra.model.fabric.PodS(fabricPodP, type='ALL', name='default')
+fabricRsPodPGrp = cobra.model.fabric.RsPodPGrp(fabricPodS, tDn='uni/fabric/funcprof/podpgrp-HOUSEKEEPING_GROUP')
+
+c = cobra.mit.request.ConfigRequest()
+c.addMo(fabricInst)
+md.commit(c)
+
+l3extDomP = cobra.model.l3ext.DomP(topMo, name='MPOD_L3_DOMAIN')
+infraRsVlanNs = cobra.model.infra.RsVlanNs(l3extDomP, tDn='uni/infra/vlanns-[MPOD_VLANS]-static')
+
+
+# commit the generated code to APIC
+#print toXMLStr(topMo)
+c = cobra.mit.request.ConfigRequest()
+c.addMo(l3extDomP)
+md.commit(c)
+
